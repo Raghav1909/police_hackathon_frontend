007ea9065008af85d83ecc1c009122f34ebcd754
alwinsDen <jsmadesimple@gmail.com>
2023-02-02 04:24:09 +0530

    Added basic filters to the app

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,15 @@
-import React, {useEffect, useState} from "react";
-import "./login.module.scss"
-import {Box, Button, Checkbox, Chip, Dialog, Fab, Input, TextField} from "@mui/material";
-import * as PropTypes from "prop-types";
+import React, {useState} from "react";
+import style from "./login.module.scss"
+import {Box, Button, Checkbox, Chip, Dialog, Fab, Input, Pagination} from "@mui/material";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCircleXmark, faCross, faMagnifyingGlass, faPlus, faXmark} from "@fortawesome/free-solid-svg-icons";
+import {
+    faAngleDown,
+    faAngleUp,
+    faCircleXmark,
+    faMagnifyingGlass,
+    faPlus,
+    faXmark
+} from "@fortawesome/free-solid-svg-icons";
 
 function AccountCircle(props) {
     return null;
@@ -99,9 +105,146 @@ const Login=()=>{
                 </div>
             </Dialog>
         </div>
-        <div>
+        {/*This here is the filter section*/}
+        <div style={{display:"flex", alignItems:"center", marginTop:"13.4px", flexWrap:"wrap"}}>
             <b>Filters:&nbsp;</b>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+            <CustomFilter/>
+        </div>
+    {/*    combined view parameters: */}
+        <div style={{display:"flex", alignItems:"center", marginTop:"6.4px", flexWrap:"wrap"}}>
+            <b>combined view parameters:&nbsp;</b>
+            <Button variant="outlined" endIcon={<FontAwesomeIcon icon={faXmark} style={{height:"10px"}}/>} style={{
+                marginRight:"13px"
+            }}>
+                valid_id | valid_id_details
+            </Button>
+            <Button variant="outlined" endIcon={<FontAwesomeIcon icon={faXmark} style={{height:"10px"}}/>} style={{
+                marginRight:"13px"
+            }}>
+                valid_id | valid_id_details
+            </Button>
+            <p>+ add new parameters</p>
+        </div>
+        <div style={{display:"flex", alignItems:"center", marginTop:"13.4px", flexWrap:"wrap"}}>
+            <p>select page:&nbsp;</p>
+            <Pagination count={4} variant="outlined" shape="rounded" />
+        </div>
+        <div style={{display:"flex", alignItems:"center", marginTop:"13.4px", flexWrap:"wrap"}}>
+            <p>rows per page:&nbsp;</p>
+            <RowCountComp/>
         </div>
     </div>
 }
+export const CustomFilter=()=>{
+    const test_vals = ['test','test','test','test','test','test','test','test','test','test']
+    const [toggleOpen,setToggleOpen]=useState(false);
+    return <div
+            style={{display:"flex",
+                alignItems:'center',
+                border: '1.03535px solid #C4C4C4',
+                borderRadius: '3.10604px',
+                position:"relative",
+                marginBottom:"7px",
+                marginRight:"13px"
+            }}>
+        <p style={{margin:"0 7.2px"}}><b >place_of_origin: </b>Bengaluru</p>
+        <div className={style.drop_down} style={{background:"#FAFAFA",padding:"5px",borderRadius: '3.10604px'}} onClick={()=> {
+            setToggleOpen(state=>!state)
+        }
+        }>
+            <FontAwesomeIcon icon={!toggleOpen?faAngleDown:faAngleUp}/>
+        </div>
+    {/*    this here is for the drop down*/}
+        {toggleOpen && <div style={{
+            width: "100%",
+            maxHeight: "200px",
+            position: "absolute",
+            background: '#ffffff',
+            top: "32px",
+            border: '1.03535px solid #C4C4C4',
+            borderRadius: '3.10604px',
+            boxShadow: "0px 3.54973px 5.3246px rgba(0, 0, 0, 0.25)",
+            overflow:"scroll",
+            zIndex:1
+        }}>
+            {
+                test_vals.map((vls, index) => {
+                    return <div
+                        className={style.drop_opt}
+                        style={{
+                            background: index == 3 ? "#6C85AB" : "",
+                            borderRadius: "2.6623px",
+                            height: "max-content",
+                            margin: "3px 3.5px",
+                            padding: "3px 0",
+                        }}
+                    ><p style={{marginLeft: "5px", color: index == 3 ? "#ffffff" : "#000000"}}>{vls}</p></div>
+                })
+            }
+        </div>}
+    </div>
+}
+
+export const RowCountComp=()=>{
+    const test_vals = [1,2,3,4,5,6,7]
+    const [toggleOpen,setToggleOpen]=useState(false);
+    return <div
+        style={{display:"flex",
+            alignItems:'center',
+            border: '1.03535px solid #C4C4C4',
+            borderRadius: '3.10604px',
+            position:"relative",
+            margin:"3.5px 13px 3.5px 0"
+        }}>
+        <p style={{margin:"0 7.2px"}}>10</p>
+        <div className={style.drop_down} style={{background:"#FAFAFA",padding:"5px",borderRadius: '3.10604px'}} onClick={()=> {
+            setToggleOpen(state=>!state)
+        }
+        }>
+            <FontAwesomeIcon icon={!toggleOpen?faAngleDown:faAngleUp}/>
+        </div>
+        {/*    this here is for the drop down*/}
+        {toggleOpen && <div style={{
+            width: "100%",
+            maxHeight: "200px",
+            position: "absolute",
+            background: '#ffffff',
+            top: "32px",
+            border: '1.03535px solid #C4C4C4',
+            borderRadius: '3.10604px',
+            boxShadow: "0px 3.54973px 5.3246px rgba(0, 0, 0, 0.25)",
+            overflow:"scroll",
+            zIndex:1
+        }}>
+            {
+                test_vals.map((vls, index) => {
+                    return <div
+                        className={style.drop_opt}
+                        style={{
+                            background: index == 3 ? "#6C85AB" : "",
+                            borderRadius: "2.6623px",
+                            height: "max-content",
+                            margin: "3px 3.5px",
+                            padding: "3px 0",
+                        }}
+                    ><p style={{marginLeft: "5px", color: index == 3 ? "#ffffff" : "#000000"}}>{vls}</p></div>
+                })
+            }
+        </div>}
+    </div>
+}
+
 export default Login
